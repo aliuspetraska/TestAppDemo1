cc56bc22e0f3aecf965f7479c32457ad4a81a23d
Alius Petraska <alius.petraska@ibm.com>
2018-10-16 13:27:33 +0300

    initial commit

---
+++ TestAppDemo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppDemo1
+//
+//  Created by Alius Petraska on 16/10/2018.
+//  Copyright © 2018 Alius Petraska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
